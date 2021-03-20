aaa599164e008efe4614fd07a1c41d0665ad6677
masahito-nagayama <me18sf67m@gmail.com>
2021-03-21 01:57:04 +0900

    カラムの追加

--- clothes-app/database/migrations/2021_03_18_135130_create_products_table.php
+++ clothes-app/database/migrations/2021_03_18_135130_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('descripiton');
+            $table->string('image');
+            $table->integer('price');
             $table->timestamps();
         });
     }
