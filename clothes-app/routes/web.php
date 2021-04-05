4402746564b12362591adf61852713cd2999ce13
masahito-nagayama <me18sf67m@gmail.com>
2021-04-05 09:15:13 +0900

    create lineitemcontroller

--- clothes-app/routes/web.php
+++ clothes-app/routes/web.php
@@ -5,3 +5,8 @@ Route::name('product.')
     Route::get('/', 'ProductController@index')->name('index');
     Route::get('/product/{id}', 'ProductController@show')->name('show');
   });
+
+Route::name('line_item.')
+  ->group(function(){
+    Route::post('/lineitem/create','LineItemController@create')->name('create');
+  });
